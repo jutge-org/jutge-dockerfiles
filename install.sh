d9added081c22499245999d0c24261c7c5d61426
Jordi Petit <jpetit@cs.upc.edu>
2020-03-12 13:15:32 +0100

    Shrink LaTeX packages and others

--- install.sh
+++ install.sh
@@ -28,25 +28,22 @@ ln -fs /usr/share/zoneinfo/Europe/Andorra /etc/localtime
 dpkg-reconfigure --frontend noninteractive tzdata
 
 # Install common tools
-apt-get --yes install imagemagick joe nano python python-pip python-cairo python-yaml python3 python3-pip python3-cairo python3-yaml tree wget
+apt-get --yes install imagemagick joe nano python3 python3-pip python3-pillow python3-cairo python3-yaml tree wget
 
 # Install common python3 packages
-pip3 install jutge pillow jutge-problems-toolkit
-
-# Install common python packages
-pip install jutge pillow
+pip3 install jutge jutge-toolkit
 
 # Install common compilers
-apt-get --yes install build-essential clang openjdk-8-jdk-headless python python3
+apt-get --yes install build-essential
 
 # Install exotic compilers
 if [ $1 = "full" ] || [ $1 = "server" ] ; then
-    apt-get --yes install algol68g basic256 beef bf bwbasic chicken-bin libchicken-dev clisp erlang f2c fpc gambc gccgo gdc gfortran ghc gnat gobjc golang gprolog guile-2.0 lua5.3 nodejs ocaml-native-compilers r-base r-base-core r-base-dev r-cran-vgam  r-recommended rhino ruby stalin tcl php-cli
+    apt-get --yes install algol68g basic256 beef bf bwbasic clang chicken-bin libchicken-dev clisp erlang f2c fpc gambc gccgo gdc gfortran ghc gnat gobjc golang gprolog guile-2.0 lua5.3 nodejs ocaml-native-compilers openjdk-8-jdk-headless r-base r-base-core r-base-dev r-cran-vgam  r-recommended rhino ruby stalin tcl php-cli
 fi
 
 # Install latex
 if [ $1 = "full" ] || [ $1 = "lite" ] ; then
-    apt-get --yes install texlive-full
+    apt-get --yes install texlive-latex-extra texlive-games texlive-pstricks
 fi
 
 # Clean apt
