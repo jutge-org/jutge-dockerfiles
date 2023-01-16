9389c8b5d87b2f7dc848ac796bf2848e726024e7
Jutge.org Labs <contact@jutge.org>
2023-01-16 16:42:59 +0100

    past changes

--- install.sh
+++ install.sh
@@ -35,8 +35,11 @@ apt-get --yes install build-essential clang
 # Install python3
 apt-get --yes install python3 python3-pip python3-pillow python3-cairo python3-yaml
 
-# Install python3 packages
-pip3 install jutge jutge-toolkit easyinput
+# Install python3 jutge packages
+pip3 install jutge jutge-toolkit easyinput yogi turtle-pil pytokr
+
+# Install more python3 packages
+pip3 install mypy pycodestyle
 
 # Install jutge-vinga
 jutge-install-vinga
@@ -44,8 +47,9 @@ jutge-install-vinga
 # Install exotic compilers
 if [[ $1 = "full" ]] || [[ $1 = "server" ]] ; then
     curl -sSL https://dist.crystal-lang.org/apt/setup.sh | sudo bash
-    apt-get --yes install algol68g basic256 beef bf bwbasic chicken-bin libchicken-dev clisp erlang f2c fpc gambc gccgo gdc gfortran ghc gnat gobjc golang gprolog guile-2.0 lua5.3 nodejs ocaml-native-compilers openjdk-8-jdk-headless r-base r-base-core r-base-dev r-cran-vgam r-recommended rhino ruby stalin tcl php-cli rustc crystal nim
-    jutge-install-verilog
+    apt-get --yes install algol68g basic256 beef bf bwbasic chicken-bin libchicken-dev clisp erlang f2c fpc gambc gccgo gdc gfortran ghc gnat gobjc golang gprolog guile-2.2 lua5.3 nodejs ocaml-native-compilers openjdk-8-jdk-headless r-base r-base-core r-base-dev r-cran-vgam r-recommended rhino ruby stalin tcl php-cli rustc crystal
+    # does not work: apt-get --yes install nim    
+    # does not work: jutge-install-verilog
 fi
 
 # Install latex
