e4e37d5364aab2b02f48946b836def46811169f1
Jordi Petit <jordi.petit@gmail.com>
2024-07-17 09:36:20 +0200

    Fix adduser and add Clojure compiler

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
+# Update and upgrade apt packages
+apt-get --yes update
+apt-get --yes upgrade
+
+# Install initial tools
+apt-get --yes install tzdata imagemagick joe nano tree ack wget curl moreutils less adduser
 
 # Add users
 adduser --disabled-password --gecos '' worker
@@ -15,10 +21,6 @@ adduser --disabled-password --gecos '' worker9
 cp /root/.inputrc /home/worker
 chown -R worker:worker /home/worker
 
-# Update and upgrade apt packages
-apt-get --yes update
-apt-get --yes upgrade
-
 # Set timezone
 # https://serverfault.com/questions/949991/how-to-install-tzdata-on-a-ubuntu-docker-image
 export DEBIAN_FRONTEND=noninteractive
@@ -26,25 +28,17 @@ apt-get install --yes tzdata
 ln -fs /usr/share/zoneinfo/Europe/Andorra /etc/localtime
 dpkg-reconfigure --frontend noninteractive tzdata
 
-# Install common tools
-apt-get --yes install sudo imagemagick joe nano tree ack wget curl moreutils
-
 # Install C compilers
 apt-get --yes install build-essential clang
 
 # Install python3
-apt-get --yes install python3 python3-pip python3-pillow python3-cairo python3-yaml python3.10-venv
+apt-get --yes install python3 python3-pip python3-pillow python3-cairo python3-yaml python3-venv
 
 # Install python3 jutge packages
 pip3 install jutge-toolkit turtle-pil easyinput yogi pytokr
 
 # Install more python3 packages
 pip3 install mypy pycodestyle
-
-# Install codon
-/bin/bash -c "$(curl -fsSL https://exaloop.io/install.sh)"
-mv /root/.codon /opt/codon
-ln -s /opt/codon/bin/codon /usr/local/bin/codon
 
 # Install jutge-vinga
 jutge-install-vinga
@@ -55,6 +49,22 @@ if [[ $1 = "full" ]] || [[ $1 = "server" ]] ; then
     apt-get --yes install algol68g basic256 beef bf bwbasic chicken-bin libchicken-dev clisp erlang f2c fpc gambc gccgo gdc gfortran ghc gnat gobjc golang gprolog guile-2.2 lua5.3 nodejs ocaml-native-compilers openjdk-8-jdk-headless r-base r-base-core r-base-dev r-cran-vgam r-recommended rhino ruby stalin tcl php-cli rustc crystal
     # does not work: apt-get --yes install nim
     # does not work: jutge-install-verilog
+
+    # Install codon
+    /bin/bash -c "$(curl -fsSL https://exaloop.io/install.sh)"
+    mv /root/.codon /opt/codon
+    ln -s /opt/codon/bin/codon /usr/local/bin/codon
+
+    # Install clojure
+    mkdir /tmp/clojure
+    cd /tmp/clojure
+    apt-get --yes install rlwrap
+    curl -L -O https://github.com/clojure/brew-install/releases/latest/download/linux-install.sh
+    chmod +x linux-install.sh
+    sudo ./linux-install.sh
+    cd
+    rm -rf /tmp/clojure
+
 fi
 
 # Fix wspace protections
