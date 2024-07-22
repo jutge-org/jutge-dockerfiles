b10f14092daf0c9ce6b57120d3df8ad6e080b021
Jordi Petit <jordi.petit@gmail.com>
2024-07-22 08:45:54 +0200

    Improve

--- install.sh
+++ install.sh
@@ -38,23 +38,21 @@ apt-get --yes install build-essential clang
 apt-get --yes install python3 python3-pip python3-pillow python3-cairo python3-yaml python3-venv
 
 # Install python3 jutge packages
-pip3 install --break-system-packages jutge-toolkit turtle-pil easyinput yogi pytokr
+pip3 install --break-system-packages jutge-toolkit jutge-server-toolkit turtle-pil easyinput yogi pytokr
 
 # Install more python3 packages
 pip3 install --break-system-packages mypy pycodestyle
 
-# Install jutge-vinga
-jutge-install-vinga
+# Install jutge sudo tools
+jutge-install-sudo-tools
 
 # Install exotic compilers
 if [[ $1 = "full" ]] || [[ $1 = "server" ]] ; then
-    curl -sSL https://dist.crystal-lang.org/apt/setup.sh | sudo bash
 
-    apt-get --yes install algol68g basic256 beef bwbasic chicken-bin libchicken-dev clisp erlang f2c fpc gambc gccgo gdc gfortran ghc gnat gobjc golang gprolog guile-2.2 lua5.3 nodejs ocaml openjdk-8-jdk-headless r-base r-base-core r-base-dev r-cran-vgam r-recommended rhino ruby stalin tcl php-cli rustc crystal
+    apt-get --yes install algol68g basic256 beef bwbasic chicken-bin libchicken-dev clisp erlang f2c fpc gambc gccgo gdc gfortran ghc gnat gobjc golang gprolog guile-2.2 lua5.3 nodejs ocaml openjdk-11-jdk-headless r-base r-base-core r-base-dev r-cran-vgam r-recommended rhino ruby stalin tcl php-cli rustc crystal
 
-    # does not work: apt-get --yes install nim
-    # does not work: apt-get --yes install bf
-    # does not work: jutge-install-verilog
+    # does not work: nim
+    # does not work: bf
 
     # Install codon
     /bin/bash -c "$(curl -fsSL https://exaloop.io/install.sh)"
@@ -62,15 +60,10 @@ if [[ $1 = "full" ]] || [[ $1 = "server" ]] ; then
     ln -s /opt/codon/bin/codon /usr/local/bin/codon
 
     # Install clojure
-    mkdir /tmp/clojure
-    cd /tmp/clojure
-    apt-get --yes install rlwrap
-    curl -L -O https://github.com/clojure/brew-install/releases/latest/download/linux-install.sh
-    chmod +x linux-install.sh
-    sudo ./linux-install.sh
-    cd
-    rm -rf /tmp/clojure
+    /bin/bash -c "$(curl -fsSL https://github.com/clojure/brew-install/releases/latest/download/linux-install.sh)"
 
+    # Install crystal
+    /bin/bash -c "$(curl -fsSL https://dist.crystal-lang.org/apt/setup.sh)"
 fi
 
 # Fix wspace protections
@@ -81,27 +74,30 @@ if [[ $1 = "full" ]] || [[ $1 = "lite" ]] ; then
     apt-get --yes install texlive-latex-extra texlive-games texlive-pstricks
 fi
 
-# Clean apt
-apt-get clean
-
 # Fix convert, because it does not generate the eps files
 # see https://askubuntu.com/questions/1081695/error-during-converting-jpg-to-pdf
 perl -p -i.bak -e 's/domain="coder" rights="none"/domain="coder" rights="read|write"/g' /etc/ImageMagick-6/policy.xml
 
 # Install python problem libraries
+if [[ $1 = "full" ]] || [[ $1 = "server" ]] ; then
 
-mkdir /opt/pylibs
-cd /opt/pylibs
+    mkdir /opt/pylibs
+    cd /opt/pylibs
 
-pylibs="numpy scipy simpy networkx optilog pandas matplotlib more-itertools biopython beautifulsoup4"
+    pylibs="numpy scipy simpy networkx optilog pandas matplotlib more-itertools biopython beautifulsoup4"
 
-for pylib in $pylibs
-do
-    echo install $pylib
-    python3 -m venv $pylib
-    source $pylib/bin/activate
-    pip install $pylib
-    pip show $pylib > $pylib/show.txt
-    deactivate
-    chmod 700 $pylib    # protect the lib
-done
+    for pylib in $pylibs
+    do
+        echo install $pylib
+        python3 -m venv $pylib
+        source $pylib/bin/activate
+        pip install $pylib
+        pip show $pylib > $pylib/show.txt
+        deactivate
+        chmod 700 $pylib    # protect the lib
+    done
+fi
+
+# Clean shit
+pip3 cache purge
+apt-get clean
