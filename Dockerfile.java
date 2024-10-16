81b7805b0fc3468042511cb6e04155b32485f442
Pau Fernández <pau.fernandez@upc.edu>
2024-10-16 19:49:07 +0200

    Changed to debian:12.7-slim
    Also:
    - standardized on "apt-get --yes"
    - fixed jq query in Zig installation.
    - Change OpenJDK from 11 to 17... XD (11 is not available anymore).

--- Dockerfile.java
+++ Dockerfile.java
@@ -1,6 +1,6 @@
 FROM jutgeorg/base:latest
 USER root
-RUN apt --yes install openjdk-11-jdk-headless
+RUN apt-get --yes install openjdk-17-jdk-headless
 ADD https://github.com/clojure/brew-install/releases/latest/download/linux-install.sh /root/install-clojure.sh
 RUN bash /root/install-clojure.sh
 USER worker
