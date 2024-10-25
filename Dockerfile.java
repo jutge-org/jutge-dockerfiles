e13e760e3aed46f4d326f508fdecdf3c18ffba6f
Witixin <73248264+Witixin1512@users.noreply.github.com>
2024-10-25 11:22:02 +0200

    Add kotlin

--- Dockerfile.java
+++ Dockerfile.java
@@ -1,6 +1,9 @@
 FROM jutgeorg/base:latest
 USER root
-RUN apt-get --yes install openjdk-17-jdk-headless
+RUN apt update && apt --yes install openjdk-17-jdk-headless
 ADD https://github.com/clojure/brew-install/releases/latest/download/linux-install.sh /root/install-clojure.sh
 RUN bash /root/install-clojure.sh
+
+RUN apt update && apt install --yes kotlin
+
 USER worker
