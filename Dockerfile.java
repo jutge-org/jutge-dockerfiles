fb57ae1eab93157830566b0aa5f4583038e9ff4a
Witixin1512 <73248264+Witixin1512@users.noreply.github.com>
2024-11-14 10:14:04 +0100

    Add Clojure and NodeJS

--- Dockerfile.java
+++ Dockerfile.java
@@ -1,9 +1,28 @@
 FROM jutgeorg/base:latest
 USER root
 RUN apt update && apt --yes install openjdk-17-jdk-headless
+
+RUN apt update && apt --yes install kotlin
+
+# Required for clj to work
+RUN apt --yes install rlwrap
 ADD https://github.com/clojure/brew-install/releases/latest/download/linux-install.sh /root/install-clojure.sh
 RUN bash /root/install-clojure.sh
 
-RUN apt update && apt install --yes kotlin
+#Make sure clojure downloads the libraries it needs
+RUN mkdir /clojure
+WORKDIR /clojure
+
+RUN echo '{:paths ["src"]}' > deps.edn
+RUN mkdir src
+RUN echo '(ns core)\n\n(println "Hello, World")' > src/core.clj
+RUN clj -M
+RUN mv /root/.m2/repository/ /clojure/.m2
+
+RUN chmod -R o+r /clojure/
+
+RUN rm /clojure/deps.edn
+RUN rm /clojure/src -rf
 
 USER worker
+WORKDIR /home/worker
